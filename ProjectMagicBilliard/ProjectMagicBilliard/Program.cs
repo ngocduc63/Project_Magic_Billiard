f636d85d61ebef2aacc7b12eed424c8c96eec54d
Nguyễn Ngọc Đức <duco060303@gmail.com>
2023-09-11 21:19:09 +0700

    Create form login

--- ProjectMagicBilliard/ProjectMagicBilliard/Program.cs
+++ ProjectMagicBilliard/ProjectMagicBilliard/Program.cs
@@ -16,7 +16,7 @@ namespace ProjectMagicBilliard
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Scene.Login());
         }
     }
 }
